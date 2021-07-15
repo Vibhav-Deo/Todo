8944ff03feef1f045549c59eb72873234504cc96
Vibhav Deo <vibhavdeo10@gmail.com>
2021-07-15 22:19:03 +1000

    Basic folder structure

--- Todo/Todo.Contracts/Events/User/UserCreatedEvent.cs
+++ Todo/Todo.Contracts/Events/User/UserCreatedEvent.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Todo.Contracts.Enums;
 
 namespace Todo.Contracts.Events.User
 {
-    class UserCreatedEvent
+    public class UserCreatedEvent : IBaseEvent
     {
+        public Guid EntityModifiedId { get; }
+        public string Message { get; }
+        public string EntityThatTookAction { get; }
+        public DateTimeOffset CreatedOn { get; }
+        public EntityType EntityType { get; }
+
+        public UserCreatedEvent(Guid entityId, string entity, EntityType entityType, DateTimeOffset createdOn)
+        {
+            EntityModifiedId = entityId;
+            EntityThatTookAction = entity;
+            CreatedOn = createdOn;
+            EntityType = entityType;
+            Message = "User list created";
+        }
     }
 }
